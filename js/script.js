04af9f336492a996294d9079e2c902683002c7ac
nana <im.gisellanana@gmail.com>
2024-08-29 13:28:43 +0700

    welcome text

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
 
 function validateForm() {
-    
+
     const username = document.forms['message-form']['username'].value;
     const birthDate = document.forms['message-form']['birth-date'].value;
     const gender = document.forms['message-form']['gender'].value;
     const message = document.forms['message-form']['message'].value;
-    
 
-    // Menggunakan operator pembanding (== atau ===) bukan operator assignment (=)
+
+    // jika inputan kosong maka ada notification alert
     if (username === "" || birthDate === "" || gender === "" || message === "") {
         alert("Tidak boleh ada yang kosong");
         return false;
     }
 
     setSenderUI(username, birthDate, gender, message);
+    updateWelcomeMessage(username); //memanggil fungsi updateWelcomeMessage 
+
     return false; // Mencegah form untuk reload halaman
+
 }
 
 function setSenderUI(username, birthDate, gender, message) {
-    // Memperbaiki kesalahan ketik 'usernamename' menjadi 'username'
     document.getElementById("sender-full-name").innerHTML = username;
     document.getElementById("sender-birth-date").innerHTML = birthDate;
     document.getElementById("sender-gender").innerHTML = gender;
     document.getElementById("sender-message").innerHTML = message;
 }
+//fungsi untuk menampilkan username ke dalam teks 
+function updateWelcomeMessage(username) {
+    const welcomeMessage = document.getElementById('welcome-message');
+    welcomeMessage.textContent = ` ${username}`;
+}
+
+// Event listener to handle form submission
+document.getElementById('message-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    validateForm(); // memanggil fungsi validateForm
+});
