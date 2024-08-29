1e082ac92e5570a2ac2d9d2db05d9dc4ea8dae60
nana <im.gisellanana@gmail.com>
2024-08-29 21:14:09 +0700

    finish fix

--- js/script.js
+++ js/script.js
@@ -19,7 +19,41 @@ function validateForm() {
     return false; // Mencegah form untuk reload halaman
 
 }
+let indexSlide = 1
+showBanner(indexSlide);
 
+//fungsi untuk mengganti slide manual
+function nextSlide(n) {
+    showBanner(indexSlide += n)
+}
+//fungsi untuk menampilkan slide berdasarkan indeks
+function showBanner(indexBanner) {
+    console.log(indexSlide)
+    let listImage = document.getElementsByClassName('banner-image');
+
+    //reset ke slide pertama jika indeks lebih besar dari jumlah gambar
+    if (indexBanner > listImage.length) {
+        indexSlide = 1;
+    }
+    //jika indeks lebih kecil dari 1, kembali ke slide terakhir
+    if (indexBanner < 1) {
+        indexSlide = listImage.length;
+    }
+
+    //sembunyikan semua gambar
+    let index = 0;
+    while (index < listImage.length) {
+        listImage[index].style.display = 'none';
+        index++;
+    }
+    // tampilkan slide yang sesuai dengan indexSlide
+    listImage[indexSlide - 1].style.display = 'block';
+}
+
+//auto slide pindah setiap 5 s
+setInterval(function(){
+    nextSlide(1); 
+},5000)
 function setSenderUI(username, birthDate, gender, message) {
     document.getElementById("sender-full-name").innerHTML = username;
     document.getElementById("sender-birth-date").innerHTML = birthDate;
